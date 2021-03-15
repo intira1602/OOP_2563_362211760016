7d365098c6520e3e282ed7372fdcebfdbeb4b6c9
lab3 <intira1798@gmail.com>
2021-03-15 17:41:59 +0700

    OOP

---
+++ src/LAB8/ILight.java
@@ -0,0 +1,11 @@
+package LAB8;
+
+public interface ILight extends extreamILight{
+    public  void turnOn();
+    public  void turnOff();
+
+}
+
+interface  extreamILight {
+    public void autoHiBeam();
+}
